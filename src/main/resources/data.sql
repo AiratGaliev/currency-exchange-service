d871a4f756f1242428d5507686f303a1be4597bd
Airat Galiev <airat.galiev@gmail.com>
2020-12-03 20:16:57 +0300

    added jpa and initialized data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values (1000, 'USD', 'RUB', 75, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values (1001, 'EUR', 'RUB', 90, 0);
