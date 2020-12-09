350add27af98d0eb507b2d8699436489d74177ef
Airat Galiev <airat.galiev@gmail.com>
2020-12-09 21:42:28 +0300

    implemented spring cloud sleuth

--- src/main/java/com/github/airatgaliev/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/github/airatgaliev/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.github.airatgaliev.currencyexchangeservice.controller;
 
 import com.github.airatgaliev.currencyexchangeservice.model.ExchangeValue;
 import com.github.airatgaliev.currencyexchangeservice.repository.ExchangeValueRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
   @Value("${server.port}")
   private int port;
@@ -20,6 +24,7 @@ public class CurrencyExchangeController {
   public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
     ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
     exchangeValue.setPort(port);
+    logger.info("{}", exchangeValue);
     return exchangeValue;
   }
 }
